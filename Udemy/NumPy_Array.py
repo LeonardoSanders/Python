72719a114d8f4cae861e4207b7164a834c65c9dd
Leonardo Sanders <leonardosanders54@gmail.com>
2024-08-20 17:15:08 -0400

    Conteúdo sobre Numpy

---
+++ Udemy/NumPy_Array.py
@@ -0,0 +1,13 @@
+''' NumPy Arrays (Matrizes) é um pacote de Python que lida com números.
+Numpy array is a powerful N-dimensional array object which is in the form
+of rows and columns. We can initialize NumPy arrays from nested Python
+lists and access it elements.
+'''
+# Numpy Array unidimensional
+import numpy as np
+a = np.array([1, 2, 3])
+print(a)
+
+# Numpy array bidimensional
+b = np.array([(1, 2, 3), (4, 5, 6)])
+print(b)
