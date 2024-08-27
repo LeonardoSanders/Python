0082976e12451ffbf20d166b45453546e369784d
Leonardo Sanders <leonardosanders54@gmail.com>
2024-08-26 21:10:49 -0400

    Aula 179

--- Udemy/Teste.py
+++ Udemy/Teste.py
@@ -1,8 +1,5 @@
-from itertools import zip_longest as zl
-lista_a = [10, 2, 3, 4, 5]
-lista_b = [12, 2, 3, 6, 50, 60, 70]
-lista_c = [
-    (x + y)
-    for x, y in zl(lista_a, lista_b, fillvalue=0)
-]
-print(lista_c)
+fat = 1
+for n in range(1, 6):
+    fat *= n
+
+print(fat)
