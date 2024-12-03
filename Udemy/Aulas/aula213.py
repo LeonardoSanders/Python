4ceb2ea775259e95482a51424652dfe4e354a198
Leonardo Sanders <leonardosanders54@gmail.com>
2024-12-02 21:33:20 -0400

    Finalização da aula 213

--- Udemy/Aulas/aula213.py
+++ Udemy/Aulas/aula213.py
@@ -5,3 +5,33 @@
 # - p/ habilitar setter
 # - p/ executar ações ao obter um atributo
 # Código cliente - é o código que usa seu código
+# É importante destacar que o @property não salva o valor, ele apenas retorna.
+
+class Caneta:
+
+    def __init__(self, cor):
+        self._cor = cor
+
+# Private ou Protected em Python é uma convenção para tornar certas variáveis, como (_cor), protegidas, de forma que outras pessoas que forem
+# utilizar o código, não manipulem tais variáveis, apenas o desenvolvedor original.
+
+
+    @property
+    def cor(self):
+        print('PROPERTY')
+        return self._cor
+    
+
+    @cor.setter
+    def cor(self, valor):
+        print('Estou no SETTER!', valor)
+        self._cor = valor
+
+# Ao passo que o metodo @property serve para retornar um valor de um atributo, utilizando um método. o Método @***.getter serve para
+# atribuir a uma variável um determinado valor, que se comportarám como um atributo quando fora da classe, como no exemplo: caneta.cor = 'Rosa'
+
+caneta = Caneta('Azul')
+print(caneta.cor)
+
+caneta.cor = 'Rosa'
+print(caneta.cor)
