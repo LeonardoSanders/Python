46a4ac1e2dc3c5c118ed350862193d8a3aba1da5
LeonardoSanders <leonardosanders54@gmail.com>
2025-02-07 18:35:38 -0400

    Att da resolucao do ex_aula256

--- Exercícios/Udemy/Otavio Miranda/ex_aula256.py
+++ Exercícios/Udemy/Otavio Miranda/ex_aula256.py
@@ -19,12 +19,15 @@ class Conta(ABC):
 
 class ContaCorrente(Conta):
 
-    def sacar(self, valor):
-        if valor > self.saldo:
-            print("O valor desejado excede o Saldo da Conta")
+    def sacar(self, saque, valor):
+        if saque:
+            if valor > self.saldo:
+                print("O valor desejado excede o Saldo da Conta!")
+            else:
+                self.saldo - valor
+                print(f'Saque de {valor} realizado. Seu saldo atual é de R$ {self.saldo}')
         else:
-            self.saldo - valor
-            return f'Saque de {valor} realizado. Seu saldo atual é de R$ {self.saldo}'
+            print('Você nao tem autorizacao para sacar!')
 
     
 class ContaPoupanca(Conta):
@@ -33,7 +36,7 @@ class ContaPoupanca(Conta):
         if valor > self.saldo:
             print("O valor desejado excede o Saldo da Conta")
         else:
-            self.saldo - valor
+            self.saldo -= valor
             return f'Saque de {valor} realizado. Seu saldo atual é de R$ {self.saldo}'
     
 
@@ -42,7 +45,6 @@ class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
-
 
 
 class Cliente(Pessoa):
@@ -58,25 +60,46 @@ class Cliente(Pessoa):
 
 class Banco:
 
-    def __init__(self, agencia):
+    def __init__(self, agencia, num_contas):
         self.agencia = agencia
+        self.num_conta = num_contas
         self.clientes = []
         self.contas = []
 
     def definir_clientes(self, *clientes):
         for cliente in clientes:
             self.clientes.append(cliente)
-        return f'Cliente {self.clientes.nome} definido com sucesso!'
+        
 
     def definir_contas(self, *contas):
         for conta in contas:
             self.contas.append(conta)
-        return f'{self.contas.__class__.__name__} definida com sucesso!'
+        
     
-    def autenticar(self) -> bool:
-        ...
+    def autenticar(self, nome) -> bool:
+        verificador = 0
+        for conta in self.contas:
+            if conta.agencia == self.agencia:
+                verificador += 1
+            if conta.numero == self.num_conta:
+                verificador += 1
+        for cliente in self.clientes:
+            if cliente.nome == nome:
+                verificador += 1
+        if verificador == 3:
+            print('Dados autenticados com sucesso. Saque autorizado!')
+            return True
+        else:
+            print('Erro na autenticacão dos dados. Saque não autorizado!')
+            return False
+
 
 
 Cliente_1 = Cliente('Leonardo', 30)
 conta_corrente_1 = ContaCorrente(2000, 5000, 2905, 921938)
 Cliente_1.definir_tipo_conta(conta_corrente_1)
+Banco_1 = Banco(2905, 921938)
+Banco_1.definir_contas(conta_corrente_1)
+Banco_1.definir_clientes(Cliente_1)
+saque = Banco_1.autenticar(Cliente_1.nome)
+conta_corrente_1.sacar(saque, 1000)
