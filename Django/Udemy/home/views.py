4c44f065b2ee1041804ece55747b3cb8191b2ccd
Leonardo Sanders <leonardosanders54@gmail.com>
2025-05-20 22:30:11 -0400

    :Att ate a aula 437 de Django

--- Django/Udemy/home/views.py
+++ Django/Udemy/home/views.py
@@ -1,6 +1,7 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+
+context = {'texto': 'Olá Mundo!'}
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome Home!')
+    return render(request, 'home/home.html', context)
